99b8396495dfd187bbc138d2819cda8bf2f32554
asrytikov <asrytikov@gmail.com>
2022-04-10 11:56:24 +0400

    Lesson 5

--- script/script.js
+++ script/script.js
@@ -2,4 +2,7 @@ $(document).ready(function(){
     $('.bxslider').bxSlider({
         pager: true, auto: true,
     });
+    $('.bxslider_team').bxSlider({
+        pager: true, controls: false,
+    });
 });
